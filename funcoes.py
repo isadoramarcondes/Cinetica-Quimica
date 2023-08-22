e6984c1da19926a250d69686c19e5383326e360a
Marcos de Cerqueira Leite Pimentel <marcos220057@ilum.cnpem.br>
2023-08-22 14:19:16 -0300

    agoravai2

---
+++ funcoes.py
@@ -0,0 +1,44 @@
+import numpy as np
+import random as rd
+from particula import Particula
+
+def step(particulas, step, tamanho):
+    """Calcula um passo da simulação para cada partícula"""
+    
+    # Detect edge-hitting and collision of every particle
+    for i in range(len(particulas)):
+        particulas[i].calc_col_parede(step,tamanho)
+        for j in range(i+1,len(particulas)):
+                particulas[i].calc_col(particulas[j],step)    
+
+                
+    # Compute position of every particle  
+    for particula in particulas:
+        particula.update(step)
+        
+        
+def lista_inicial(N, raio, massa, tamanho_caixa):
+    """Gerar uma lista de partículas inicial"""
+    particulas = []
+
+    for i in range(N):
+        
+        v_mag = np.random.rand(1)*20
+        v_ang = np.random.rand(1)*2*np.pi
+        v = np.append(v_mag*np.cos(v_ang), v_mag*np.sin(v_ang))
+        
+        colisao = True
+        while(colisao == True):
+            
+            colisao = False
+            pos = raio + np.random.rand(2)*(tamanho_caixa-2*raio) 
+            nova_particula = Particula(pos, v, raio, massa)
+            for j in range(len(particulas)):
+
+                colisao = nova_particula.checar_col(particulas[j] )
+
+                if colisao == True:
+                    break
+
+        particulas.append(nova_particula)
+    return particulas
---
+++ particula.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+class Particula:
+    
+    def __init__(self, pos, velocidade, raio, massa):
+        """inicializar o objeto partícula
+        
+        Args:
+        posição da partícula
+        velocidade da particula
+        raio da particula
+        massa da particula
+        """
+        self.massa = massa
+        self.raio = raio
+        
+        # estado atual da partícula
+        self.pos = np.array(pos)
+        self.velocidade = np.array(velocidade)
+        
+        # estado gravado ao longo de toda a simulação
+        self.trapos = [np.copy(self.pos)]
+        self.travel = [np.copy(self.velocidade)]
+        self.travel_mag = [np.linalg.norm(np.copy(self.velocidade))]
+        
+    def update(self, step):
+        """Compute position of next step."""
+        self.pos += step * self.velocidade
+        self.trapos.append(np.copy(self.pos)) 
+        self.travel.append(np.copy(self.velocidade)) 
+        self.travel_mag.append(np.linalg.norm(np.copy(self.velocidade))) 
+        
+
+    def checar_col(self, particula):
+        """Checa se houve colisão com alguma partícula"""
+        
+        r1, r2 = self.raio, particula.raio
+        x1, x2 = self.pos, particula.pos
+        di = x2-x1
+        norm = np.linalg.norm(di)
+        if norm-(r1+r2)*1.1 < 0:
+            return True
+        else:
+            return False
+
+    def calc_col(self, particula, step):
+        """calcula o resultado da colisão"""
+        m1, m2 = self.massa, particula.massa
+        r1, r2 = self.raio, particula.raio
+        v1, v2 = self.velocidade, particula.velocidade
+        x1, x2 = self.pos, particula.pos
+        di = x2-x1
+        norm = np.linalg.norm(di)
+        if norm-(r1+r2)*1.1 < step*abs(np.dot(v1-v2, di))/norm:
+            self.velocidade = v1 - 2. * m2/(m1+m2) * np.dot(v1-v2, di) / (np.linalg.norm(di)**2.) * di
+            particula.velocidade = v2 - 2. * m1/(m2+m1) * np.dot(v2-v1, (-di)) / (np.linalg.norm(di)**2.) * (-di)
+            
+
+    def calc_col_parede(self, step, tamanho):
+        """Calcula colisão com a parede
+        Args:
+        tamanho do step de tempo
+        tamanho da caixa
+        """
+        r, v, x = self.raio, self.velocidade, self.pos
+        projx = step*abs(np.dot(v,np.array([1.,0.])))
+        projy = step*abs(np.dot(v,np.array([0.,1.])))
+        if abs(x[0])-r < projx or abs(tamanho-x[0])-r < projx:
+            self.velocidade[0] *= -1
+        if abs(x[1])-r < projy or abs(tamanho-x[1])-r < projy:
+            self.velocidade[1] *= -1.
